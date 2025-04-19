480bf2e0d455de3c84bdd5ccf0efc87f94c82a78
Dan Loaiza <dan@resonancias.org>
2025-04-19 08:19:21 -0500

    calculate score with ace

--- src/main.cljs
+++ src/main.cljs
@@ -17,7 +17,7 @@
 (def suits #{"C" "D" "S" "H"})
 
 (defn rank->score [rank]
-  (cond (#{"J" "Q" "K"} rank) 10
+  (cond (#{"J" "Q" "K" "0"} rank) 10
         (#{"A"} rank) 11
         :else (parse-long rank)))
 
@@ -41,23 +41,23 @@
 
 (def deck nil)
 
-(def interval nil)
+;; (def interval nil)
 
-(defn swap-cards! [n]
-  (let [card (-> deck
-                 first
-                 :img)]
-    (set! counter n)
-    (change-card (str "card-" n) card))
-  (if (= 1 (count deck))
-    (js/clearInterval interval)
-    (set! deck (drop 1 deck))))
+;; (defn swap-cards! [n]
+;;   (let [card (-> deck
+;;                  first
+;;                  :img)]
+;;     (set! counter n)
+;;     (change-card (str "card-" n) card))
+;;   (if (= 1 (count deck))
+;;     (js/clearInterval interval)
+;;     (set! deck (drop 1 deck))))
 
-(defn run-interval []
-  (set! interval (js/setInterval (fn [e]
-                                   (let [n (if (>= counter 4) 1 (inc counter))]
-                                     (swap-cards! n)))
-                                 120)))
+;; (defn run-interval []
+;;   (set! interval (js/setInterval (fn [e]
+;;                                    (let [n (if (>= counter 4) 1 (inc counter))]
+;;                                      (swap-cards! n)))
+;;                                  120)))
 
 (defn player-new-row [type hand]
   (case type
@@ -71,8 +71,16 @@
       (change-card (str "card-" 3) (first hand))
       (change-card (str "card-" 4) (second hand)))))
 
+(defn ace-new-score [hand score]
+  (let [ace-count (count (filter #(= "A" (:rank %)) hand))]
+    (loop [total     score
+           ace-count ace-count]
+      (if (and (> total 21) (> ace-count 0))
+        (recur (- total 10) (dec ace-count))
+        total))))
+
 (defn new-turn []
-  (let [;; switch player
+  (let [ ;; switch player
         who (if (= player :player) :dealer :player)
 
         hand [(first deck) (second deck)]
@@ -83,9 +91,10 @@
     (js/console.log naipes)
     (set! player who)
     (player-new-row who naipes)
+    (set-score-value score)
 
-    (if (some #{"A"} (mapv #(:rank %) hand))
-      (js/console.log "you have an Ace!"))))
+    (when (some #{"A"} (mapv #(:rank %) hand))
+      (set-score-value (ace-new-score hand score)))))
 
 (defn game-run []
   (if (= 1 (count deck))
@@ -95,19 +104,18 @@
       (set! deck (drop 2 deck))
       (js/console.log (count deck)))))
 
+(defn init-eventhandlers []
+  (.addEventListener (js/document.getElementById "hitme-btn") "click"
+                     (fn []
+                       (game-run)))
+  (.addEventListener (js/document.getElementById "stay-btn") "click"
+                     (fn []
+                       (js/console.log "stay!!"))))
+
 (defn start-game []
   (set! score 0)
   (set-score-value score)
   (set! deck (shuffle all-cards)))
-
-(defn init-eventhandlers []
-  (.addEventListener (js/document.getElementById "hitme-btn") "click"
-                     (fn []
-                       (game-run)
-                       (js/console.log "hit me!!")))
-  (.addEventListener (js/document.getElementById "stay-btn") "click"
-                     (fn []
-                       (js/console.log "stay!!"))))
 
 (init-eventhandlers)
 
